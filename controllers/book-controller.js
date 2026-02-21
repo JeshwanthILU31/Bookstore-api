a776809b371281073bc44b277710f4ce8f6c931e
JeshwanthILU31 <2300033541@kluniversity.in>
2026-02-21 22:18:54 +0530

    Initial bookstore API structure with MVC architecture

---
+++ controllers/book-controller.js
@@ -0,0 +1,100 @@
+const Book = require('../models/book')
+
+const getAllBooks = async(req,res) => {
+   try{
+      const allBooks = await Book.find({})
+      if(allBooks.length > 0 ){
+        res.status(200).json({
+          success: true,
+          message:'List of books fetched successfully',
+          data: allBooks
+        })
+      }
+      else{
+        res.status(404).json({
+          success: false,
+          message: 'No Books found in collection'
+        })
+      }
+   } catch(e){
+       console.log(e)
+       res.status(500).json({
+        success: false,
+        message: 'Something went wrong please try again!'
+       })
+   }
+}
+
+const getSingleBookById = async(req,res) => {
+   try{
+      const getCurrentBookId = req.params.id 
+      const bookDetailsByID = await Book.findById(getCurrentBookId)
+      if(!bookDetailsByID){
+        return res.status(404).json({
+          success: 404,
+          message: 'Book with current Id is not found, please try with different id'
+        })
+      }
+      res.status(200).json({
+        success: true,
+        message: 'Book found Successfully',
+        data: bookDetailsByID
+      })
+   }catch(e){
+    console.log(e)
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong please try again!'
+    })
+   }
+
+}
+
+const addNewBook = async(req,res) => {
+  try{
+     const newBookFormData = req.body;
+     const newlyCreatedBook = await Book.create(newBookFormData)
+     if(newBookFormData){
+        res.status(201).json({
+            success : true,
+            message : 'Book added successfully',
+            data : newlyCreatedBook
+        })
+     }
+  }catch(e){
+    console.error(e)
+     res.status(500).json({
+        success: false,
+        message: 'Something went wrong please try again!'
+       })
+  }
+}
+
+const updateBook = async(req,res) => {
+    
+}
+
+const deleteBook = async(req,res) => {
+    try{
+       const getCurrentBookId = req.params.id 
+       const deletedBook = await Book.findByIdAndDelete(getCurrentBookId)
+       if(!deletedBook){
+        return res.status(404).json({
+          success: false,
+          message: 'Book is not found with this Id'
+        })
+       }
+       res.status(200).json({
+        success: true,
+        data: deletedBook
+       })
+    }catch(e){
+      console.log(e)
+      res.status(500).json({
+        success: false,
+        message: 'Something went wrong, please try again!'
+      })
+    }
+}
+
+module.exports = {getAllBooks,getSingleBookById,addNewBook,updateBook,deleteBook};
